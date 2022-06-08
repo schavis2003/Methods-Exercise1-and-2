2e6d5d4c6cf49fcd545611004cfe9a64cc860f79
schavis2022 <schavis2022>
2022-06-08 12:57:15 -0400

    Funny Method Ms.Piggy

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("What your name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("Where are you going");
+            string color = Console.ReadLine();
+            Console.WriteLine(color);
+            Console.WriteLine("Why are you smiling with those big teeth");
+            string animal = Console.ReadLine();
+            Console.WriteLine(animal);
+            Console.WriteLine("Why would you eat me?");
+            string band = Console.ReadLine();
+            Console.WriteLine(band);
+            
+            
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
