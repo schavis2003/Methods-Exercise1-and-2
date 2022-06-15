5056e5a95608a3bd14fe186781c3575869714f8d
schavis2022 <schavis2022>
2022-06-15 00:13:14 -0400

    Method Exercise1 and Exercise2

--- Program.cs
+++ Program.cs
@@ -4,84 +4,187 @@ namespace Methods
 {
     internal class Program
     {
+        private static object references;
+
         static void Main(string[] args)
 
         {
-            Console.WriteLine("What your name");
-            string name = Console.ReadLine();
-            Console.WriteLine(name);
-            Console.WriteLine("Where are you going");
-            string color = Console.ReadLine();
-            Console.WriteLine(color);
-            Console.WriteLine("Why are you smiling with those big teeth");
-            string animal = Console.ReadLine();
-            Console.WriteLine(animal);
-            Console.WriteLine("Why would you eat me?");
-            string band = Console.ReadLine();
-            Console.WriteLine(band);
-            
-            
-         
+            //Console.WriteLine("What your name");
+            //string name = Console.ReadLine();
+            //Console.WriteLine(name);
+
+            //Console.WriteLine("What is you favorite color");
+            //string color = Console.ReadLine();
+            //Console.WriteLine(color);
+
+            //Console.WriteLine("What is favorite animal");
+            //string animal = Console.ReadLine();
+            //Console.WriteLine(animal);
+
+            //Console.WriteLine("Whats you dogs name");
+            //string dogName = Console.ReadLine();
+            //Console.WriteLine(dogName);
+
+            //Console.WriteLine("What is you favorite band?");
+            //string band = Console.ReadLine();
+            //Console.WriteLine(band);
+
+            //Console.WriteLine($"There handsome man named {name}, his eyes were my favorite {color}.");
+            //Console.WriteLine($"He had a {animal} called {dogName}, he was also {color}.");
+            //Console.WriteLine($"The handsome man {name} and his dog {dogName} loved listening to his" +
+            //$"his favorite {band}");
+
+        }    
+
+        {
+            Console.WriteLine("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Give me a number to add to first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+        int sum = Sum(num1, num2);
+        Console.WriteLine($"The sum is: {sum}");
 
 
 
 
+            Console.WriteLine("Give a number")
+            int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Give me a number to substract from the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+        int sum = Sum(num1, num2);
+        Console.WriteLine($"The sum is");
 
 
 
+        Console.WriteLine("Give me a number");
+            int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Give me a number to multiply by the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+        int product = Multiple(num1, num2);
+        Console.WriteLine($"The sum is: {sum}");
 
 
 
+            Console.WriteLine("Give me a number to divde");
+            int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Give me a number to devide by the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+        int divide = Sum(num1, num2);
+        Console.WriteLine($"The sum is: {sum}");
 
 
 
+    {public static int Sum(params int[] list)
+
+            for(int i =0: i < list.Lengt: i ++>)
+            {
+               sum = Sum + list[i]             
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
     }
+
+
+
+
+
+
+            
+
+
+
+           
+
+            
+        }   
+
+
+
+
+
+
+
+          
+
+          
+    
+    }
+           
+
+           
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
 }
+    
